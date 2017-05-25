a8f1d9120961b0491b481a8bf0dedc78b6c24202
Florian Stravock <florian@xer0.design>
2017-05-25 22:30:55 +0100

    Update scrolltext.py

--- rpi-rgb-led-matrix/examples-api-use/scrolltext.py
+++ rpi-rgb-led-matrix/examples-api-use/scrolltext.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-text = (("This is a test", (0,255,255)), (" Squig is no craic", (255,0,255)))
+text = (("This is a test", (0,255,255)), (" did it work?", (255,0,255)))
