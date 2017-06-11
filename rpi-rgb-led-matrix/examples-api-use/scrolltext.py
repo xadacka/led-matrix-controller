d08ad16a53b3fa9032e764d7606c2cd5a34ee4e2
root <root@raspberrypi>
2017-06-11 22:47:02 +0000

    added video

--- rpi-rgb-led-matrix/examples-api-use/scrolltext.py
+++ rpi-rgb-led-matrix/examples-api-use/scrolltext.py
@@ -1,1 +1,4 @@
-text = (("No ", (0, 0, 0)), ("Content", (0, 0, 0)))
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+text = (("welcome ", (0,255,255)), ("To The Brogue", (255,0,255)))
