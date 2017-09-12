5e661db8580463902b09d89fefdf96e30f6411de
Florian Stravock <florian@xadacka.com>
2017-09-13 00:07:30 +0100

    image upgrades

--- rpi-rgb-led-matrix/examples-api-use/scrolltext.py
+++ rpi-rgb-led-matrix/examples-api-use/scrolltext.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-text = (("", (128,128,128)), ("We are the champions!", (128,128,128)))
+text = (("", (128,128,128)), ("Welcome to The Brogue ", (128,128,128)))
